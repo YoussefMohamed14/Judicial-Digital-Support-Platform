2075fab3ebab2b733c819f7b5f8d64ba3396eeae
FARES <amxfares@gmail.com>
2026-06-25 15:55:23 +0300

    added Lawyers ViewModel only because they are Application user , their controller (LawyersController) and test view that will be changed in the future

---
+++ JDSP/ViewModel/LawyerListViewModel.cs
@@ -0,0 +1,12 @@
+﻿//it has view model only because they are also ApplicationUsers
+
+namespace JDSP.ViewModel
+{
+    public class LawyerListViewModel
+    {
+        public string LawyerId { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int CaseId { get; set; }
+    }
+}
