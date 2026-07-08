84357ef01752876e05a0e24caba26b3efc3dbc97
Youssef Mohamed <ym051618@gmail.com>
2026-07-09 00:20:53 +0300

    delete

--- JDSP/ViewModel/LawyerSearchViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JDSP.ViewModels.Lawyers {
-    public class LawyerSearchViewModel {
-        public string? SearchTerm { get; set; }
-
-        public string? Specialization { get; set; }
-
-        public List<LawyerListItemViewModel> Lawyers { get; set; } = new();
-    }
-}
